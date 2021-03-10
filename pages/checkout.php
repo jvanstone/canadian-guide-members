8242c65164ea4a90b10e980149f1ff67ae12d6ff
jvanstone <jason@vanstoneonline.com>
2021-03-10 12:09:52 -0500

    refund

--- pages/checkout.php
+++ pages/checkout.php
@@ -563,7 +563,7 @@
 						$tos = "";
 					}
 				?>
-				<div class="col-10 ml-4"><input class="form-check-input" type="checkbox" name="tos" value="1" id="tos" <?php checked( 1, $tos ); ?> /> <label class="col-6 form-check-label" class="<?php echo pmpro_get_element_class( 'pmpro_label-inline pmpro_clickable', 'tos' ); ?>" for="tos"><?php printf(__('I agree to the %s', 'paid-memberships-pro' ), $tospage->post_title);?></label></div>
+				<div class="col-10 ml-4"><input class="form-check-input" type="checkbox" name="tos" value="1" id="tos" <?php checked( 1, $tos ); ?> /> <label class="form-check-label" class="<?php echo pmpro_get_element_class( 'pmpro_label-inline pmpro_clickable', 'tos' ); ?>" for="tos"><?php printf(__('I agree to the %s', 'paid-memberships-pro' ), $tospage->post_title);?></label></div>
 			</div> <!-- end pmpro_checkout-fields -->
 		</div> <!-- end pmpro_tos_fields -->
 		<?php
